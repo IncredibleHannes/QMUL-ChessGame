21381a155819545d80ca5afa43464943d96080df
Johannes Hartmann <johannes.Hartmann.Calw@web.de>
2017-11-27 00:56:57 +0000

    added knight movements

--- src/Model/Chessman/Knight.cpp
+++ src/Model/Chessman/Knight.cpp
@@ -1,4 +1,5 @@
 #include "Knight.h"
+#include "Board.h"
 
 Knight::Knight(Chessman::Colour colour, Position position)
   : Chessman(colour, position) {
@@ -8,7 +9,17 @@ Knight::Knight(Chessman::Colour colour, Position position)
 std::list<Move> Knight::getPossibleMoves(const Board& board) const {
   std::list<Move> moves;
   if(!isCaptured()) {
+    int x = this->getCurrentPosition().getX();
+    int y = this->getCurrentPosition().getY();
+    std::list<Position> positions = {
+      Position(x + 2, y - 1), Position(x + 1, y - 2), Position(x + 2, y + 1),
+      Position(x + 1, y + 2), Position(x - 2, y - 1), Position(x - 1, y - 2),
+      Position(x - 2, y + 1),  Position(x - 1, y + 2)};
 
+    for(Position p : positions) {
+      if (p.isValid())
+        moves.push_back(Move(Position(x, y), p));
+    }
   }
   return moves;
 }
