7854f4e4788601b196fce69d66a6b047103c7fed
Johannes Hartmann <johannes.Hartmann.Calw@web.de>
2017-11-27 01:02:59 +0000

    fixed that queen cant capture own figures

--- src/Model/Chessman/Queen.cpp
+++ src/Model/Chessman/Queen.cpp
@@ -23,81 +23,113 @@ std::list<Move> Queen::getPossibleMoves(const Board& board) const {
 
     while (top || right || bottom || left) {
       if (top) {
-        if (x + i >= 0 && x + i < 8 && y >= 0 && y < 8) {
-          if(board.getChessman(Position(x + i, y)) != nullptr){
+        Position p = Position(x + i, y - i);
+        if (p.isValid()) {
+          if(board.getChessman(p) != nullptr){
             top = false;
+            if (board.getChessman(p)->getColour() != this->colour)
+              moves.push_back(Move(Position(x, y), p));
+          } else {
+            moves.push_back(Move(Position(x, y), p));
           }
-          moves.push_back(Move(Position(x, y), Position(x + i, y)));
         } else {
           top = false;
         }
       }
       if (right) {
-        if (x >= 0 && x< 8 && y + i >= 0 && y + i < 8) {
-          if(board.getChessman(Position(x, y + i)) != nullptr){
+        Position p = Position(x + i, y + i);
+        if (p.isValid()) {
+          if(board.getChessman(p) != nullptr){
             right = false;
+            if (board.getChessman(p)->getColour() != this->colour)
+              moves.push_back(Move(Position(x, y), p));
+          } else {
+            moves.push_back(Move(Position(x, y), p));
           }
-          moves.push_back(Move(Position(x, y), Position(x, y + i)));
         } else {
           right = false;
         }
       }
       if (bottom) {
-        if (x - i >= 0 && x - i < 8 && y >= 0 && y < 8) {
-          if(board.getChessman(Position(x - i,y)) != nullptr){
+        Position p = Position(x - i, y - i);
+        if (p.isValid()) {
+          if(board.getChessman(p) != nullptr){
             bottom = false;
+            if (board.getChessman(p)->getColour() != this->colour)
+              moves.push_back(Move(Position(x, y), p));
+          } else {
+            moves.push_back(Move(Position(x, y), p));
           }
-          moves.push_back(Move(Position(x, y), Position(x - i, y)));
         } else {
           bottom = false;
         }
       }
       if (left) {
-        if (x >= 0 && x < 8 && y - i >= 0 && y - i < 8) {
-          if(board.getChessman(Position(x, y - i)) != nullptr){
+        Position p = Position(x - i, y + i);
+        if (p.isValid()) {
+          if(board.getChessman(p) != nullptr){
             left = false;
+            if (board.getChessman(p)->getColour() != this->colour)
+              moves.push_back(Move(Position(x, y), p));
+          } else {
+            moves.push_back(Move(Position(x, y), p));
           }
-          moves.push_back(Move(Position(x, y), Position(x, y - i)));
         } else {
           left = false;
         }
       }
       if (leftTop) {
-        if (x + i >= 0 && x + i < 8 && y - i >= 0 && y - i < 8) {
-          if(board.getChessman(Position(x + i,y - i)) != nullptr){
+        Position p = Position(x + i, y - i);
+        if (p.isValid()) {
+          if(board.getChessman(p) != nullptr){
             leftTop = false;
+            if (board.getChessman(p)->getColour() != this->colour)
+              moves.push_back(Move(Position(x, y), p));
+          } else {
+            moves.push_back(Move(Position(x, y), p));
           }
-          moves.push_back(Move(Position(x, y), Position(x + i, y - i)));
         } else {
           leftTop = false;
         }
       }
       if (rightTop) {
-        if (x + i >= 0 && x + i < 8 && y + i >= 0 && y + i < 8) {
-          if(board.getChessman(Position(x + i,y + i)) != nullptr){
+        Position p = Position(x + i, y + i);
+        if (p.isValid()) {
+          if(board.getChessman(p) != nullptr){
             rightTop = false;
+            if (board.getChessman(p)->getColour() != this->colour)
+              moves.push_back(Move(Position(x, y), p));
+          } else {
+            moves.push_back(Move(Position(x, y), p));
           }
-          moves.push_back(Move(Position(x, y), Position(x + i, y + i)));
         } else {
           rightTop = false;
         }
       }
       if (leftBottom) {
-        if (x - i >= 0 && x - i < 8 && y - i >= 0 && y - i < 8) {
-          if(board.getChessman(Position(x - i,y - i)) != nullptr){
+        Position p = Position(x - i, y - i);
+        if (p.isValid()) {
+          if(board.getChessman(p) != nullptr){
             leftBottom = false;
+            if (board.getChessman(p)->getColour() != this->colour)
+              moves.push_back(Move(Position(x, y), p));
+          } else {
+            moves.push_back(Move(Position(x, y), p));
           }
-          moves.push_back(Move(Position(x, y), Position(x - i, y - i)));
         } else {
           leftBottom = false;
         }
       }
       if (rightBottom) {
-        if (x - i >= 0 && x - i < 8 && y + i >= 0 && y + i < 8) {
-          if(board.getChessman(Position(x - i,y + i)) != nullptr){
+        Position p = Position(x - i, y + i);
+        if (p.isValid()) {
+          if(board.getChessman(p) != nullptr){
             rightBottom = false;
+            if (board.getChessman(p)->getColour() != this->colour)
+              moves.push_back(Move(Position(x, y), p));
+          } else {
+            moves.push_back(Move(Position(x, y), p));
           }
-          moves.push_back(Move(Position(x, y), Position(x - i, y + i)));
         } else {
           rightBottom = false;
         }
