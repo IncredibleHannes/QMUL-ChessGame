7ce9d1f7f21eea7ae62fc3eb8983ab2baa06076f
Johannes Hartmann <johannes.Hartmann.Calw@web.de>
2017-12-12 13:24:09 +0000

    added documentation for UserAction

--- src/Model/UserAction.h
+++ src/Model/UserAction.h
@@ -1,3 +1,13 @@
+/**
+    ChessGame
+    UserAction.h
+    Purpose: This class representing the actions a user can make in this game.
+    Useractions can be of different types.
+
+
+    @author Johannes Hartmann
+    @version 1.0 2017-12-12
+*/
 #ifndef USERACTION_H
 #define USERACTION_H
 
@@ -6,12 +16,26 @@
 class UserAction {
 public:
   enum ActionType { MoveAction, Load, Save, Quit, Undo, Invalid};
-
+  /**
+   * Constructor should only be used if the type is not MoveAction
+   * @param type Sets the type of the action
+   */
   UserAction(ActionType type);
+  /**
+   * Constructor if the type is MoveAction
+   * @param move representing the move that will be done with this action
+   */
   UserAction(Move *move);
+  /**
+   * Desctuctor deletes the type is a MoveAction
+   */
   ~UserAction();
 
   ActionType getType() const;
+  /**
+   * Getter for the move
+   * @return Move pointer if the type is MoveAction or nullptr for each other type
+   */
   Move* getMove() const;
 
 private:
