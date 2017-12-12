fe6406510c72952127359d2ec5582622d27c7fe4
Johannes Hartmann <johannes.Hartmann.Calw@web.de>
2017-12-12 14:45:25 +0000

    added main documentation

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,15 @@
+/**
+    <h1>main</h1>
+    <h3>Main.cpp</h3><br>
+    This class is representing the controller. The whole game is implemented
+    in the Model View Controller style. The main implements the whole game cicle.
+    It has to create the board, check if its check/checkmate/draw and after this
+    handle the user actions
+
+
+    @author Johannes Hartmann
+    @version 1.0 2017-12-12
+*/
 #include "Model/Board.h"
 #include "View/View.h"
 #include "Model/Move.h"
