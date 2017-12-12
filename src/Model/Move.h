59801a73ad3593cd4c31e3afbf539c79fd6c9cb1
Johannes Hartmann <johannes.Hartmann.Calw@web.de>
2017-12-12 13:11:48 +0000

    added move documentation

--- src/Model/Move.h
+++ src/Model/Move.h
@@ -14,17 +14,63 @@ public:
     Casteling = 0x02,
     Promotion = 0x04
   };
-
+  /**
+   * Standart constructor with a origin and taget. Sets move type to normal
+   * @param origin Origin of the move
+   * @param target Target of the move
+   */
   Move(Position origin, Position target);
+  /**
+   * Standart constuctor with a aditional option to set a type
+   * @param origin Origin of the move
+   * @param target Target of the move
+   * @param type   Type of the move
+   */
   Move(Position origin, Position target, MoveType type);
+  /**
+   * Copy constructor witch copies the caured chessman aswell if its not a nullptr
+   * @param move Move to copy
+   */
   Move(const Move &move);
+  /**
+   * Desturctor deletes everything including the capured chessman if not nullptr
+   */
   ~Move();
+  /**
+   * Getter for origin
+   * @return Origin of the mvoe
+   */
   Position getOrigin() const;
+  /**
+   * Getter for target
+   * @return target of the move
+   */
   Position getTarget() const;
+  /**
+   * Adds a type to the move.
+   * @param type Type to add to the move
+   */
   void addType(MoveType type);
+  /**
+   * Checks if the move is of the given type
+   * @param  type Type to check against
+   * @return      True if the move has this type
+   */
   bool hasType(MoveType type) const;
+  /**
+   * Returns a pointer to the captured chessman if the move is a capture. If
+   * not it returns a nullptr
+   * @return Pointer to chessman
+   */
   Chessman* getCapturedChessman() const;
+  /**
+   * Adds the type capture to the move and stores the given chassmanpointer.
+   * @param chessman The chessman tha was captured
+   */
   void setCaptureMove(Chessman *chessman);
+  /**
+   * Equals opertator overloaded
+   */
   bool operator==(const Move &toCompare);
 private:
   Position origin;
