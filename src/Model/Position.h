233164136149be5ea8b5785eef5a77fc38b9eec3
Johannes Hartmann <johannes.Hartmann.Calw@web.de>
2017-12-12 13:17:09 +0000

    added documentation for position

--- src/Model/Position.h
+++ src/Model/Position.h
@@ -5,17 +5,47 @@
 
 class Position {
 public:
-  // Constructors
+  /**
+   * Constructor initialses each value with -1
+   */
   Position();
+  /**
+   * Constructor initialses each value with the given values. There is no
+   * validity check for the given values
+   * @param x Coordinate starting at 0
+   * @param y Coordinate starting at 0
+   */
   Position(int x, int y);
+  /**
+   * A given position string will be transforemd to the actual corresponding values.
+   * There is no validity check for the given values
+   * @param y Position String
+   */
   Position(std::string y);
+  /**
+   * Copy constructor for a given Position. The default implementation is used
+   * @param other Position to copy
+   */
   Position(const Position &other) = default;
 
+  /**
+   * Getter for the X coordinate
+   * @return Coordinate starting at 0
+   */
   int getX() const;
+  /**
+   * Getter for the Y coordinate
+   * @return Coordinate starting at 0
+   */
   int getY() const;
-
+  /**
+   * Checks if the coordinates are in range [0..7]
+   * @return True if position is valid
+   */
   bool isValid() const;
-
+  /**
+   * Overloaded equals operator
+   */
   bool operator==(const Position &toCompare);
 
 private:
